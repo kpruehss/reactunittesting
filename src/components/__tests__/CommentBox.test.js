1b4c8ddc23d2c39709d2b2f62c32a97a80a9d869
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-07-06 13:18:13 +1000

    ADD: additional CommentBox tests

---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import CommentBox from 'components/CommentBox';
+
+let wrapped;
+
+beforeEach(() => wrapped = mount(<CommentBox />));
+
+afterEach(() => wrapped.unmount());
+
+it('has a text area and a button', () => {
+  wrapped = mount(<CommentBox />);
+
+  expect(wrapped.find('textarea').length).toEqual(1);
+  expect(wrapped.find('button').length).toEqual(1);
+});
+
+it('has text area user can enter text in', () => {
+  wrapped.find('textarea').simulate('change', {
+    target: { value: 'new comment' }
+  });
+  wrapped.update();
+
+});
+
+it('empties the text area when form submitted', () => {
+  wrapped.find('textarea').simulate('change', {
+    target: { value: 'new comment' }
+  });
+  wrapped.update();
+
+  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+  
+  wrapped.find('form').simulate('submit');
+  wrapped.update();
+
+  expect(wrapped.find('textarea').prop('value')).toEqual('');
+});
