c15b30bb5084e19c0108106cb534a88a14680f16
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-08-06 14:05:53 +1000

    added React Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList.js';
+import { Route } from 'react-router-dom';
 
 export default () => {
   return (
     <div>
-      <CommentBox />
-      <CommentList />
+      <Route path='/post' component={CommentBox} />
+      <Route path='/' exact component={CommentList} />
     </div>
   );
 };
